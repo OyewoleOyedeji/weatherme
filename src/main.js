244619473a9770d7aea0e3de8b5468b6fb4ad8a1
LokotamaTheMastermind <57059138+LokotamaTheMastermind@users.noreply.github.com>
2021-12-17 15:54:55 +0100

    vue-ui: added registerServiceWorker.js, user: edited main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
-document.title = 'Weatherme'
+import devtools from '@vue/devtools'
 import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import './registerServiceWorker'
+import store from './store'
+import titleMixin from "./mixins/titleMixin";
 
-createApp(App).use(VueAxios, axios).mount('#app')
+if (process.env.NODE_ENV === "development") {
+    devtools.connect('192.168.56.1', 8098)
+}
+
+createApp(App).use(store).use(VueAxios, axios).mixin(titleMixin).mount('#app')
