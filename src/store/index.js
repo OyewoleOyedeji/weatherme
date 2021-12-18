5b674ab8575010afe694ab25507859f4dea53dea
LokotamaTheMastermind <57059138+LokotamaTheMastermind@users.noreply.github.com>
2021-12-18 13:22:23 +0100

    Edit src/store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
@@ -16,14 +17,42 @@ export default createStore({
       },
       targetURL: "/v1/forecast.json",
     },
-    forecastInfo: [],
+    initialPageLoad: true,
+    online: true,
+    request: {
+      results: [],
+      error: false,
+    }
   },
   mutations: {
     setQuery(state, query) {
       state.query = query
+    },
+    setOnlinePresenceInitialPageLoad(state, presence) {
+      state.online = presence,
+        state.initialPageLoad = false
+    },
+    setRequestError(state, payload) {
+      state.request.error = payload
     }
   },
   actions: {
+    callApi(context) {
+      axios.get(context.state.settings.targetURL, {
+        baseURL: context.state.settings.requestConfig.baseURL,
+        params: {
+          days: context.state.settings.requestConfig.params.days,
+          q: context.state.query,
+          key: context.state.settings.requestConfig.params.key,
+        },
+      }).then(response => {
+        context.commit("setOnlinePresenceInitialPageLoad", true),
+          console.log(response)
+      }).catch(error => {
+        context.commit("setOnlinePresenceInitialPageLoad", false),
+          console.log(error)
+      })
+    }
   },
   modules: {
   }
