5e3d74b89776ea9c22f26b23069e8dd3c7ad9bc5
LokotamaTheMastermind <57059138+LokotamaTheMastermind@users.noreply.github.com>
2021-12-18 13:25:35 +0100

    vue-ui: create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  pwa: {
+    name: 'Weatherme',
+    themeColor: '#f13819',
+    manifestOptions: {
+      background_color: '#ffffff'
+    }
+  },
+
+  productionSourceMap: false
+}
